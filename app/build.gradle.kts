210a3dd249bf77ceea82a65a02f6dc7ebb9618e7
CraftWorksMC <thelonegamer87@gmail.com>
2024-05-19 16:13:10 +0200

    Upgrade dependencies and release build.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -67,7 +67,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
     implementation("sh.calvin.reorderable:reorderable:1.3.3")
@@ -76,9 +76,9 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.palette:palette-ktx:1.0.0")
-    implementation("androidx.media3:media3-exoplayer:1.3.0")
-    implementation("androidx.media3:media3-session:1.3.0")
-    implementation("androidx.media3:media3-ui:1.3.0")
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-session:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
     implementation("androidx.mediarouter:mediarouter:1.7.0")
     implementation("androidx.compose.material3:material3-android:1.2.1")
 
