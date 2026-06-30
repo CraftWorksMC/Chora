3ca88a64d2316a090b305da8a666c716c6e021da
Piripe <pierre.piripe@proton.me>
2026-06-30 14:29:12 +0200

    Updated StarredRepository

--- app/src/main/java/com/craftworks/music/data/repository/StarredRepository.kt
+++ app/src/main/java/com/craftworks/music/data/repository/StarredRepository.kt
@@ -1,48 +1,16 @@
-package com.craftworks.music.legacy.data.repository
+package com.craftworks.music.data.repository
 
-import androidx.media3.common.MediaItem // MediaItem can represent songs, albums (via browse MediaId), etc.
-import com.craftworks.music.data.datasource.local.LocalDataSource
-import com.craftworks.music.data.datasource.navidrome.NavidromeDataSource
-import com.craftworks.music.managers.LocalProviderManager
-import com.craftworks.music.managers.NavidromeManager
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import javax.inject.Inject
+import com.craftworks.music.data.model.LibraryType
+import com.craftworks.music.managers.MediaProviderManager
 import javax.inject.Singleton
 
-// LEGACY CODE! MUST NOT BE USED
-// TODO("Delete legacy file")
 @Singleton
-class StarredRepository @Inject constructor(
-    private val localDataSource: LocalDataSource,
-    private val navidromeDataSource: NavidromeDataSource
-) {
-
-    suspend fun getStarredItems(ignoreCachedResponse: Boolean = false): List<MediaItem> = coroutineScope {
-        val deferredStarred = mutableListOf<Deferred<List<MediaItem>>>()
-
-        if (NavidromeManager.checkActiveServers())
-            deferredStarred.add(async { navidromeDataSource.getNavidromeStarred(ignoreCachedResponse) })
-
-        if (LocalProviderManager.checkActiveFolders())
-            deferredStarred.add(async { localDataSource.getLocalStarredItems() })
-
-        deferredStarred.awaitAll().flatten()
-    }
-
-     suspend fun starItem(id: String = "", albumId: String = "", artistId: String = "", ignoreCachedResponse: Boolean) {
-         if (!id.startsWith("Local_") || !albumId.startsWith("Local_") || !artistId.startsWith("Local_"))
-             navidromeDataSource.starNavidromeItem(id, albumId, artistId, ignoreCachedResponse)
-         else
-             localDataSource.starLocalItem(id)
+class StarredRepository {
+     suspend fun starItem(ids: List<String>, type: LibraryType) {
+         MediaProviderManager.currentProvider.value?.createFavorite(ids, type);
      }
 
-     suspend fun unStarItem(id: String = "", albumId: String = "", artistId: String = "", ignoreCachedResponse: Boolean) {
-         if (!id.startsWith("Local_") || !albumId.startsWith("Local_") || !artistId.startsWith("Local_"))
-             navidromeDataSource.unstarNavidromeItem(id, albumId, artistId, ignoreCachedResponse)
-         else
-            localDataSource.unstarLocalItem(id)
+     suspend fun unStarItem(ids: List<String>, type: LibraryType) {
+         MediaProviderManager.currentProvider.value?.deleteFavorite(ids, type);
      }
 }
